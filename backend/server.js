297fc5aab4d5399ed50e94eaaacf57dcea41a42b
jonnyking-hub1 <jonathanking8304@gmail.com>
2026-09-07 15:53:30 +0100

    debug: catch silent errors

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,13 @@
+process.on('uncaughtException', (err) => {
+    console.error('UNCAUGHT EXCEPTION:', err);
+    process.exit(1);
+});
+
+process.on('unhandledRejection', (err) => {
+    console.error('UNHANDLED REJECTION:', err);
+    process.exit(1);
+});
+
 require('dotenv').config();
 const express      = require('express');
 const cookieParser = require('cookie-parser');
